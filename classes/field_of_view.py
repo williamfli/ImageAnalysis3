818ae3426d395870efb39ab58395bccb33f7cf2d
Bill Li <bill.limw@gmail.com>
2024-07-29 15:16:10 -0400

    Made import changes to fix multiprocessing bug

--- classes/field_of_view.py
+++ classes/field_of_view.py
@@ -6,13 +6,13 @@ import copy
 import numpy as np
 import pickle as pickle
 # fix mp reducer 4GB limit
-from ..required_files import pickle2reducer
+#from ..required_files import pickle2reducer
+import multiprocessing as mp
+#ctx = mp.get_context()
+#ctx.reducer = pickle2reducer.Pickle2Reducer()
+from ..required_files import pickle4reducer
 import multiprocessing as mp
 ctx = mp.get_context()
-ctx.reducer = pickle2reducer.Pickle2Reducer()
-#from ..required_files import pickle4reducer
-#import multiprocessing as mp
-#ctx = mp.get_context()
 #ctx.reducer = pickle4reducer.Pickle4Reducer()
 
 # saving
